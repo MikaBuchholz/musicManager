7f61269b41ac291dc79a5305f64ff0522d226c04
MikaBuchholz <mika.buchholz3@gmail.com>
2020-07-08 02:47:51 +0200

    Test

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from musicmanager import get_file
+from musicManager import get_file
 
 me = get_file("https://youtu.be/6Nrxc0Vdldw",r"C:\Users\mikab_42vdtq2\Downloads", r"C:\test")
 me.start_function()
